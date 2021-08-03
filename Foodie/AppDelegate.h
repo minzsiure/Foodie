0fd17f41a98399b2f63519050d4bac88400c714f
Eva Xie <xieyi@fb.com>
2021-08-03 12:23:22 -0400

    added boiler code

--- Foodie/AppDelegate.h
+++ Foodie/AppDelegate.h
@@ -6,9 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
 
 @end
 
